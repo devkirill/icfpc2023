89ff47da3697f8daeaf528ead6df65fc961ffb9f
Kirill Deviatkin <k.devyatkin@vanguardsoft.ru>
2023-07-10 00:44:00 +0500

    new version calculation

--- src/test/kotlin/icfpc/y2023/SeparateAlgorithmTestsV3.kt
+++ src/test/kotlin/icfpc/y2023/SeparateAlgorithmTestsV3.kt
@@ -1,6 +1,5 @@
 package icfpc.y2023
 
-import icfpc.y2023.db.model.Solution
 import icfpc.y2023.model.Pillar
 import icfpc.y2023.model.Point
 import icfpc.y2023.model.Solve
@@ -10,11 +9,10 @@ import icfpc.y2023.utils.readUrl
 import icfpc.y2023.utils.send
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.MethodSource
-import utils.domain
-import utils.filterBorder
-import utils.getCells
-import utils.getProblemsCount
+import utils.*
 import java.net.URL
+import java.util.*
+import kotlin.math.abs
 
 class SeparateAlgorithmTestsV3 {
     val scoring = CalcScoringService()
@@ -22,44 +20,29 @@ class SeparateAlgorithmTestsV3 {
     companion object {
         @JvmStatic
         fun ids(): List<Int> {
-            return (1..getProblemsCount()).toList().shuffled()
+//            return listOf(87, 90)
+            return (1..getProblemsCount()).toList().sortedByDescending { abs(it - 18) }//.shuffled()
         }
-    }
-
-    // @ParameterizedTest
-    // @MethodSource("ids")
-    fun test2(id: Int) {
-        val problem = Task.parse(readUrl("$domain/problem/$id"))
-        val solution = getPlaceFor(problem)
-        var score = scoring.calc(problem, solution)
-        URL("$domain/add/$id?calc=true").send(solution)
-        println("score for $id: $score")
     }
 
     @ParameterizedTest
     @MethodSource("ids")
-    fun enchancer(id: Int) {
+    fun test2(id: Int) {
         val problem = Task.parse(readUrl("$domain/problem/$id"))
-        val oldSolution = Solution.parse(readUrl("$domain/best/$id"))
-        val solved = oldSolution.contents
-        val properSolution = addVolume(problem, solved.placements)
-
-        var scoreNew = scoring.calc(problem, properSolution)
-        var scoreOld = scoring.calc(problem, solved)
-        println("scores for $id: new $scoreNew, old $scoreOld, stored ${oldSolution.score}")
-        if (scoreNew > scoreOld || scoreOld != oldSolution.score) {
-            URL("$domain/add/$id?calc=true").send(properSolution)
-            println("problem $id updated")
-        }
+        val solution = getPlaceFor(problem)
+        val score = scoring.calc(problem, solution)
+        URL("$domain/add/$id?calc=true").send(solution)
+        println("score for $id: $score")
     }
 
 
     fun getPlaceFor(problem: Task): Solve {
         val sourceCells = getCells(problem).shuffled()
         var dist = 1
-        val cells = sourceCells.filterBorder(problem, dist = dist).toMutableSet()
+        val borderCells = sourceCells.filterBorder(problem, dist = dist)
+        val cells = borderCells.toMutableSet()
         println("cellssize ${cells.size}")
-        println("cells ${cells}")
+//        println("cells ${cells}")
         val mPoints = mutableSetOf<Point>()
 
         val musicians = mutableMapOf<Int, MutableSet<Int>>()
@@ -72,23 +55,24 @@ class SeparateAlgorithmTestsV3 {
         val pillars = problem.pillars.map { Pillar(it.center[0], it.center[1], it.radius) }.toMutableList()
         val result = mutableMapOf<Int, Point>()
 
-        while (mPoints.size < problem.musicians.size && cells.isNotEmpty()) {
-            val list = cells.map { mPos ->
-                val (score, instr) = musicians.map { (instr, musicians) ->
-                    val score = scoring.calcWithPillars(problem, instr, mPos, pillars)
-                    score to instr
-                }.maxByOrNull { it.first }!!
-                Triple(mPos, score, instr)
-            }
+        val data = borderCells.pmap { mPos ->
+            val otherPillars = borderCells.filter { it != mPos }.map { Pillar(it.x, it.y, 5.0) }
+            musicians.keys.map { instr ->
+                val score = scoring.calcWithPillars(problem, instr, mPos, pillars + otherPillars)
+                score to instr
+            }.map { Triple(mPos, it.first, it.second) }
+        }.flatten()
 
-            val (cell, score, instr) = list.maxBy { (_, l, _) -> l }
+        while (mPoints.size < problem.musicians.size && cells.isNotEmpty()) {
+            val (cell, score, instr) = data.filter { it.first !in mPoints && it.third in musicians }
+                .maxByOrNull { (_, l, _) -> l } ?: break
             if (score <= 0) {
-                continue
+                break
             }
             cells -= cell
             musicians[instr]!!.let { set ->
                 val index = set.first()
-                println("found best place for $instr on $cell")
+                println("found best place for $instr on $cell at $score")
                 mPoints += cell
                 result[index] = cell
                 set.remove(index)
@@ -96,25 +80,21 @@ class SeparateAlgorithmTestsV3 {
                     musicians.remove(instr)
                 }
             }
-
         }
-        for (m in musicians.)
 
-            for (m in problem.musicians) {
-                if (cells.isEmpty()) {
-                    dist++
-                    cells += sourceCells.filterBorder(problem, dist = dist)
-                        .filter { it !in mPoints }
+        musicians.values.flatten().let { notFilled ->
+            if (notFilled.isNotEmpty()) {
+                println("not filled ${notFilled.size} cells")
+                val notUsed =
+                    LinkedList(sourceCells.filter { it !in mPoints }.sortedBy { p -> mPoints.minOf { p dist it } })
+                notFilled.forEach {
+                    result[it] = notUsed.removeFirst()
                 }
-                val cell = cells.maxBy { mPos ->
-                    problem.attendees.map({ scoring.calcSimpleInfluence(m, mPos, it) }).sum()
-                }
-                println("found best place for $m on $cell")
-                cells -= cell
-                mPoints += cell
             }
-        val placement = mPoints.toList()
-        return addVolume(problem, placement)
+        }
+
+
+        return addVolume(problem, result.entries.sortedBy { it.key }.map { it.value })
     }
 
     fun addVolume(problem: Task, placement: List<Point>): Solve {
